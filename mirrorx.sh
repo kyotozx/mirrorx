00145df3374ef9d5dfee155a20a1f71d790685d3
kyotozx <134347770+kyotozx@users.noreply.github.com>
2024-01-22 22:08:00 -0300

    Update mirrorx.sh

--- mirrorx.sh
+++ mirrorx.sh
@@ -1,4 +1,4 @@
-#!/bin/bash 
+#!/bin/bash
 
 command_exists() {
     command -v "$1" >/dev/null 2>&1
@@ -17,50 +17,75 @@ print_banner() {
        ░    ░     ░        ░         ░ ░     ░         ░    ░  made by @kyotozx
                                                                
 "
-echo "
+    echo "
 Mirror X is a simple script that can clone a website and return with what the user wants.
 "
 }
 
-if ! command_exists wget || ! command_exists grep; then
-echo "Mirror X error: Please make sure that wget and grep are installed."
-exit 1
-fi
-
 if [ -z "$1" ]; then
-print_banner
-echo "
-Usage: ./mirrorx.sh <URL>"
-exit 1 
+    print_banner
+    echo "
+    Usage: ./mirrorx.sh <URL>"
+    exit 1 
 fi 
 
+
+if [ -z "$1" ]; then
+  echo "Usage: ./mirrorx.sh <URL>"
+  exit 1
+fi
+
 url="$1"
+if [[ ! $url =~ ^https?:// ]]; then
+  echo "Please enter a valid URL (starting with http:// or https://)"
+  exit 1
+fi
 
 print_banner
-echo -n "Do you want to dowload images? (y/n): "
-read dowload_images
-
-if [ "$downlaod_images" == "y" ]; then 
-wget -mk -e robots=off "$url"
-else
 wget -m -e robots=off "$url"
+
+
+grep_string=$(read -p "Enter the file types to search for (e.g., .js .php .txt .json .py .css): ")
+
+
+files=$(find . -name "*.$grep_string" -type f)
+
+
+if [ -z "$files" ]; then
+  echo "No files found with the specified extension."
+else
+  for file in $files; do
+    echo "$file"
+  done
 fi
 
-wget -m "$url" 
 
-if [ $? -ne 0 ]; then
-echo "Error: wget failed. Check the url."
-exit 1
-fi
- 
-read -p "Enter the file types to search for (e.g. .js .php .txt .json .py .css): " grep_string
-grep -r --include="*$grep_string" .
-echo $grep_string
+while true; do
+  read -p "Do you want to delete the downloaded files (y/n)? " remove_files
+  if [ "$remove_files" == "y" ]; then
+   
+    filename=$(basename "$url")
 
-echo -n "Do you want to remove the dowloaded files? (y/n): "
-read remove_files
-
-if [ "$remove_files" == "y" ]; then 
-rm -rf "$url"
-echo "Downloaded files removed."
-fi
+    if [[ -w "$filename" ]]; then
+      rm -rf "$filename"
+      echo "File $filename deleted."
+    else
+      echo "You do not have permission to delete the file $filename."
+    fi
+    break
+  elif [ "$remove_files" == "n" ]; then
+    read -p "Do you want to create a compressed archive of the downloaded files (y/n)? " create_archive
+    if [ "$create_archive" == "y" ]; then
+    
+      filename=$(basename "$url")
+    
+      zip -r "$filename.zip" "$filename"
+      echo "Archive $filename.zip created."
+      break
+    else
+      break
+    fi
+  else
+    echo "Invalid input. Please enter y or n."
+  fi
+done
